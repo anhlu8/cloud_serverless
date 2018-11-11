75e3da2b20513823915e411f039bdb8d7d900d5e
Anh Lu <anhlucci@outlook.com>
2018-11-11 15:18:43 -0500

    added AWS RDS MySQL

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE If EXISTS redqueendb;
+CREATE DATABASE redqueendb;
+USE redqueendb;
+
+CREATE TABLE allData (
+    id INT NOT NULL AUTO_INCREMENT,
+    PRIMARY KEY (id),
+    
+)
+
+SELECT * FROM allData;
--- src/worker.js
+++ src/worker.js
@@ -8,35 +8,32 @@ const awsAccountId = process.env.AWS_ACCOUNTID;
 const sqsQueueName = process.env.SQS_QUEUE_NAME;
 const queueUrl = `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${awsAccountId}/${sqsQueueName}`;
 const giphyAPI = process.env.GIPHY_API;
-async function sendMessagetoSQS(event, context, done) {
+
+module.exports.sendMessagetoSQS =  async function(event, context, callback){
     const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${giphyAPI}&q=cat&limit=25&offset=0&rating=G&lang=en`);
     const responseJSON = await response.json();
     const params = {
-        MessageBody: "Hello World",
+        MessageBody: "this is message sent",
         QueueUrl: queueUrl,
         MessageAttributes: {
-            "Title": {
-                DataType: "String",
-                StringValue: "The Whistler"
+            "Data": {
+                DataType: "Binary",
+                StringValue: responseJSON
             },
-            "Author": {
-                DataType: "String",
-                StringValue: "John Grisham"
-            },
-            "WeeksOn": {
-                DataType: "Number",
-                StringValue: "6"
-            }
         },
     };
     sqs.sendMessage(params, function (err, data) {
         if (err) {
             console.log('error:', 'Fail send message' + err);
             const response = {
-                statusCode: 500,
+                statusCode: 400,
                 body: JSON.stringify({
                     message: 'Error'
-                })
+                }),
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    "Access-Control-Allow-Headers": 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'
+                }
             };
             callback(null, response);
         } else {
@@ -45,11 +42,25 @@ async function sendMessagetoSQS(event, context, done) {
                 statusCode: 200,
                 body: JSON.stringify({
                     message: data.MessageId
-                })
+                }),
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    "Access-Control-Allow-Headers": 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token'
+                }
             }
             callback(null, response);
         }
     })
 };
 
-module.export.sendMessagetoSQS;
+module.export.receiveMessagefromSQS = async function(event, context, callback){
+    const params = {
+        MessageBody: "this is message received",
+        QueueUrl: queueUrl,
+        MaxNumberOfMessages: 1,
+    };
+
+    sqs.receiveMessage(params, function(err, data){
+
+    })
+};
