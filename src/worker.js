98dc758bd6f844360a1e0f765e5e3bc05a600879
Anh Lu <anhlucci@outlook.com>
2018-11-10 20:45:17 -0500

    fetch to giphy and update params to send to SQS

--- src/worker.js
+++ src/worker.js
@@ -6,12 +6,28 @@ const sqs = new AWS.SQS({
 });
 const awsAccountId = process.env.AWS_ACCOUNTID;
 const sqsQueueName = process.env.SQS_QUEUE_NAME;
-const queueUrl = `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${awsAccountId}/${sqsQueueName}`
-
+const queueUrl = `https://sqs.${process.env.AWS_REGION}.amazonaws.com/${awsAccountId}/${sqsQueueName}`;
+const giphyAPI = process.env.GIPHY_API;
 async function sendMessagetoSQS(event, context, done) {
+    const response = await fetch(`https://api.giphy.com/v1/gifs/search?api_key=${giphyAPI}&q=cat&limit=25&offset=0&rating=G&lang=en`);
+    const responseJSON = await response.json();
     const params = {
         MessageBody: "Hello World",
-        QueueUrl: queueUrl
+        QueueUrl: queueUrl,
+        MessageAttributes: {
+            "Title": {
+                DataType: "String",
+                StringValue: "The Whistler"
+            },
+            "Author": {
+                DataType: "String",
+                StringValue: "John Grisham"
+            },
+            "WeeksOn": {
+                DataType: "Number",
+                StringValue: "6"
+            }
+        },
     };
     sqs.sendMessage(params, function (err, data) {
         if (err) {
